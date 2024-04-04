d138c5f8d757ace214e65f3d4e3265f531e5d9c8
BryanGarcia25 <125710066+BryanGarcia25@users.noreply.github.com>
2024-04-03 17:41:35 -0700

    adding information about me and installing icons from Font Awesome

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export default class AboutComponent {
-
+  public iconAcademic = faGraduationCap;
+  public iconJob = faBriefcase;
 }
