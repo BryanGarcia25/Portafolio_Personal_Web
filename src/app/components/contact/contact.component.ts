57c82f7f836ffc5a3e2b214d0856bb63e4fe62c4
BryanGarcia25 <125710066+BryanGarcia25@users.noreply.github.com>
2024-04-10 01:07:25 -0700

    adding information in the Contact section

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faFax } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export default class ContactComponent {
-
+  public iconEmail = faEnvelope;
 }
