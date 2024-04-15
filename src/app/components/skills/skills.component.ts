d494c40edb8767ab9dfa49bec48c6e4a49180e1e
BryanGarcia25 <125710066+BryanGarcia25@users.noreply.github.com>
2024-04-15 16:40:04 -0700

    Making changes in the skills section
    Removing the service and json file to create a single interface in charge of displaying the information in the section

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { routes } from '../../app.routes';
 import { Tecnology } from '../../interfaces/tecnology_interface';
-import { TecnologiesService } from '../../services/tecnologies.service';
+import { tecnologies } from '../../interfaces/tecnologies'
 
 @Component({
   selector: 'app-skills',
@@ -15,20 +15,8 @@ export default class SkillsComponent {
   public menuSkills = routes.map(route => route.children).flat().filter(route => route?.path);
 
   public tecnologiesList: Tecnology[] = [];
-  public tecnologiesService: TecnologiesService = inject(TecnologiesService);
 
   constructor() {
-    this.tecnologiesService.getTecnologies().subscribe(res => {
-      for (let index = 0; index < res.length; index++) {
-        this.tecnologiesList.push(res[index]);
-      }
-      this.sortTecnologiesList();
-    })
-  }
-
-  sortTecnologiesList() {
-    this.tecnologiesList.sort((a,b) => {
-      return b.category.localeCompare(a.category)
-    });
+    this.tecnologiesList = tecnologies;
   }
 }
